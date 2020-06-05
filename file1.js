ef2fb2a82c2f9ce33da861ade598301c2b2ccf9b
Nooruddin1993 <57154209+Nooruddin1993@users.noreply.github.com>
2020-06-05 11:40:16 -0400

    2
    2

--- file1.js
+++ file1.js
@@ -1,5 +1,13 @@
 // for loop works by iterating a set of instructiins a certain specified number of times
-
-for(i=0; i<10 ;i++) {
-    console.log("Hello Happy Summer!!!");
+function a(){
+    sum = 0;
+    for(i=0; i<10 ;i++) {
+        console.log("Hello Happy Summer!!!" +i);
+        sum += i;
+        if (i==5) {
+            break;
+        }
+    }
+return sum;
 }
+console.log("sum is", a());
